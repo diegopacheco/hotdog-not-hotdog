e42ca6ab198f3a3c3dc9777677605a5c3ade5d23
Shubham Kalra <iam@shubhamkalra.com>
2017-09-21 22:32:02 -0600

    website handling with flask

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# hotdog-not-hotdog
+# hotdog not hotdog  <a href="url"><img src="http://www.emoji.co.uk/files/apple-emojis/food-drink-ios/381-hot-dog.png"  height="48" width="48" ></a>
+
+## Inspired from Silicon Valley tv show's Entrepreneur in residence Jiyan Yang's app 
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/ACmydtFDTGs/0.jpg)](https://www.youtube.com/watch?v=ACmydtFDTGs)
+
+
+
+* Demo - URL
+
+
+* Here is an image classifier built to learn about various food types - including Hotdogs, Burgers, Pizza and Sandwiches. 
+
+
+*  Unless image has high inclination to Hotdog I'm not marking it as hotdog. 
+
+
+* Training it only on hotdog was giving lower accuracy than training it on various similar food items and checking if the food item is a hotdog 
+
+* Tensorflow is used to retrain MobileNet with a concept called Transfer learning. 
+  * MobileNets are optimized to be small and efficient, at the cost of some accuracy, when compared to other pre-trained models
+  * Transfer Learning, means starting with a model that has been already trained on another problem. Deep learning from scratch can take days, but transfer learning can be done in short order.
+
+* List of all [Pre-trained models](https://github.com/tensorflow/models/tree/master/slim#pre-trained-models) one can use to build an image classifier depending on usage and compute available
+
+
+*  Demo hosted on Google App Engine using Flask 
+
+
+## Potential
+* Product #1: With enough training size and compute strength - Anyone can extend this to create the See-food App/ Shazam for food
+* Product #2: Tie food to ingredients - knowing habits of food preference Amazon can reccomend ingredients when they look for food on Yelp. 
+* Product #3: Knowing your allergies an app can point camera to the food and we lookup ingredients and warn the user
---
+++ main.py
@@ -0,0 +1,188 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START app]
+
+
+
+from __future__ import print_function # In python 2.7
+import sys
+
+
+import logging
+import os, glob
+from flask import Flask, request, redirect, url_for, render_template, flash, session
+from werkzeug.utils import secure_filename
+from google.cloud import storage
+import pdb
+
+script_dir = os.path.dirname(__file__)
+
+app = Flask(__name__)
+# app.secret_key = os.urandom(19)
+
+app.debug = False
+
+
+if not app.debug:
+
+    # Do this as it's production
+    # Configure this environment variable via app.yaml
+    CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+else:
+    CLOUD_STORAGE_BUCKET = 'test_bucket_1190'
+    
+
+
+
+
+
+@app.route('/foo', methods=['POST'])
+def foo():
+    """Process the uploaded file and upload it to Google Cloud Storage."""
+    uploaded_file = request.files.get('file')
+
+    if not uploaded_file:
+        return redirect(url_for('index')) 
+        # return 'No file uploaded.', 400
+
+    # if not app.debug:
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(uploaded_file.filename)
+
+    blob.upload_from_string(
+        uploaded_file.read(),
+        content_type=uploaded_file.content_type
+    )
+    
+    file_perma_url = blob.public_url
+    #Process the file and calculate the result
+    
+    # print(uploaded_file, file=sys.stderr)
+    result = 'Hotdog!'
+
+    return render_template('foo.html',  messages={'main':result, 'second':file_perma_url })
+
+    # list_of_files = glob.glob(UPLOAD_FOLDER+'/*')
+    # latest_file = max(list_of_files, key=os.path.getctime)
+    # try:
+    #     image_loc = session['file_location']
+    #     print(image_loc, file=sys.stderr)
+    # except:
+    #     image_loc = ''
+    # result = 'Hotdog!'
+    
+    # if (image_loc == ''):
+    #     return redirect(url_for('index')) 
+    # else:
+
+'''<!doctype html>
+<title>Hello from Flask</title>
+<h1>Hello World!</h1>
+<img src="latest_file">
+
+'''
+
+@app.route('/')
+def index():
+    # if request.method == 'POST':
+    #     # check if the post request has the file part
+    #     if 'file' not in request.files:
+    #         flash('No file part')
+    #         return redirect(request.url)
+    #     file = request.files['file']
+    #     # if user does not select file, browser also
+    #     # submit a empty part without filename
+    #     if file.filename == '':
+    #         flash('No selected file')
+    #         return redirect(request.url)
+    #     if file and allowed_file(file.filename):
+    #         filename = secure_filename(file.filename)
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #         # return redirect(url_for('uploaded_file',filename=filename))
+    #         file1 = os.path.join(UPLOAD_FOLDER,filename)
+    #         session['file_location'] = file1
+    #         return redirect(url_for('foo'))
+    return render_template('home.html')
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('uploaded_file',filename=filename))
+            
+            return redirect(url_for('foo'))
+
+    # if request.method == 'GET':
+    #     return redirect(url_for('foo'))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+#testing stuff
+
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END app]
